88cd54df34d275fb29c481a214a0951bb85304e4
Mohamed Osoble <ossoble360@gmail.com>
2023-09-14 19:16:33 -0400

    Weee

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code here!
+const element = document.createElement("div");
+const newHeader = document.createElement("h1");
+element.textContent = "You've changed what's on the screen!";
+newHeader.id = "victory"
+newHeader.textContent = "Mohamed O is the champion"
+document.body.append(newHeader);
+
